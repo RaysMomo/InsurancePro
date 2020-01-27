3ec3eadecead0c6c41c7a2aaacb810e8c086c030
Ray.C <584604354@qq.com>
2020-01-27 15:40:06 +0800

    Trans done

--- InsuranceCompany/admin.py
+++ InsuranceCompany/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import TransRange
+from .models import TransRange, TransType, TransMap
 
 
 # Register your models here.
@@ -9,4 +9,15 @@ class TransRangeAdmin(admin.ModelAdmin):
     list_display = ['range_name', 'sort_code', 'status']
 
 
+class TransTypeAdmin(admin.ModelAdmin):
+    list_display = ['type_name', 'sort_code', 'status']
+
+
+class TransMapAdmin(admin.ModelAdmin):
+    list_display = ['trans_range', 'trans_type', 'status', ]
+    list_display_links = ['trans_range', 'trans_type']
+
+
 admin.site.register(TransRange, TransRangeAdmin)
+admin.site.register(TransType, TransTypeAdmin)
+admin.site.register(TransMap, TransMapAdmin)
--- InsuranceCompany/models.py
+++ InsuranceCompany/models.py
@@ -17,26 +17,31 @@ class TransRange(models.Model):
         verbose_name_plural = '运输范围'
 
 
-# class TransType(models.Model):
-#     status_choices = (('Active', u'生效'),)
-#     type_name = models.CharField(max_length=10, unique=True, verbose_name='运输方式')
-#     sort_code = models.IntegerField(verbose_name='显示排序')
-#     status = MultiSelectField(u'状态', choices=status_choices, blank=True)
-#     trans_range = models.ManyToManyField(TransRange, through='TransMap')
-#
-#     def __str__(self):
-#         return self.type_name
-#
-#     class Meta:
-#         verbose_name = '运输方式'
-#         verbose_name_plural = '运输方式'
-#
-#
-# class TransMap(models.Model):
-#     status_choices = (('Active', u'生效'),)
-#     trans_range = models.ForeignKey(TransRange, on_delete=models.CASCADE)
-#     trans_type = models.ForeignKey(TransType, on_delete=models.CASCADE)
-#     status = MultiSelectField(u'状态', choices=status_choices, blank=True)
+class TransType(models.Model):
+    status_choices = (('Active', u'生效'),)
+    type_name = models.CharField(max_length=10, unique=True, verbose_name='运输方式')
+    sort_code = models.IntegerField(verbose_name='显示排序')
+    status = MultiSelectField(u'状态', choices=status_choices, blank=True)
+    trans_range = models.ManyToManyField(TransRange, through='TransMap')
 
-    # class Meta:
-    #     db_table =
+    def __str__(self):
+        return self.type_name
+
+    class Meta:
+        verbose_name = '运输方式'
+        verbose_name_plural = '运输方式'
+
+
+class TransMap(models.Model):
+    status_choices = (('Active', u'生效'),)
+    trans_range = models.ForeignKey(TransRange, on_delete=models.CASCADE, verbose_name='运输范围')
+    trans_type = models.ForeignKey(TransType, on_delete=models.CASCADE, verbose_name='运输方式')
+    status = MultiSelectField(u'状态', choices=status_choices, blank=True)
+
+    def __str__(self):
+        return self.trans_type.__str__() + '-' + self.trans_range.__str__()
+
+    class Meta:
+        verbose_name = '运输范围与运输方式'
+        verbose_name_plural = '运输范围与运输方式'
+        unique_together = ('trans_range', 'trans_type',)
