ac37f8a08e6146d78045cbf4b979429f937c0dd0
Ray.C <584604354@qq.com>
2020-01-28 14:57:27 +0800

    InsuranceCompany & InsuranceCompanyRate done

--- InsuranceCompany/admin.py
+++ InsuranceCompany/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from .models import TransRange, TransType, TransMap
+from .models import InsuranceCompany, InsuranceCompanyRate
 
 
 # Register your models here.
@@ -18,6 +19,20 @@ class TransMapAdmin(admin.ModelAdmin):
     list_display_links = ['trans_range', 'trans_type']
 
 
+class InsuranceCompanyAdmin(admin.ModelAdmin):
+    list_display = ['company_name', 'contact', 'telephone', 'email']
+
+
+class InsuranceCompanyRateAdmin(admin.ModelAdmin):
+    list_display = ['insurance_company', 'trans_map', 'rate']
+    list_display_links = ['insurance_company', 'trans_map']
+
+
 admin.site.register(TransRange, TransRangeAdmin)
 admin.site.register(TransType, TransTypeAdmin)
 admin.site.register(TransMap, TransMapAdmin)
+admin.site.register(InsuranceCompany, InsuranceCompanyAdmin)
+admin.site.register(InsuranceCompanyRate, InsuranceCompanyRateAdmin)
+
+admin.site.site_title = "某宝后台管理"
+admin.site.site_header = "某宝后台管理"
--- InsuranceCompany/models.py
+++ InsuranceCompany/models.py
@@ -2,7 +2,6 @@ from django.db import models
 from multiselectfield import MultiSelectField
 
 
-# Create your models here.
 class TransRange(models.Model):
     status_choices = (('Active', u'生效'),)
     range_name = models.CharField(max_length=10, unique=True, verbose_name='运输范围')
@@ -45,3 +44,34 @@ class TransMap(models.Model):
         verbose_name = '运输范围与运输方式'
         verbose_name_plural = '运输范围与运输方式'
         unique_together = ('trans_range', 'trans_type',)
+
+
+class InsuranceCompany(models.Model):
+    status_choices = (('Active', u'生效'),)
+    company_name = models.CharField(max_length=100, unique=True, verbose_name='保险公司')
+    contact = models.CharField(max_length=20, verbose_name='联系人')
+    telephone = models.CharField(max_length=100, verbose_name='联系电话')
+    email = models.EmailField(verbose_name='联系人邮箱')
+    status = MultiSelectField(u'状态', choices=status_choices, blank=True)
+
+    def __str__(self):
+        return self.company_name
+
+    class Meta:
+        verbose_name = '保险公司'
+        verbose_name_plural = '保险公司'
+
+
+class InsuranceCompanyRate(models.Model):
+    insurance_company = models.ForeignKey(InsuranceCompany, on_delete=models.CASCADE, verbose_name='保险公司')
+    trans_map = models.ForeignKey(TransMap, on_delete=models.CASCADE, verbose_name='运输范围与方式')
+    rate = models.DecimalField(verbose_name='费率', decimal_places=6, max_digits=7)
+
+    def __str__(self):
+        return self.insurance_company.__str__() + '-' + self.trans_map.__str__()
+
+    class Meta:
+        verbose_name = '保险公司费率'
+        verbose_name_plural = '保险公司费率'
+
+
